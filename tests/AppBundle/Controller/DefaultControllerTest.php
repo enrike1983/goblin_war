d1ac94187b92fb515a7da8ec14d138a917acb0cf
Enrico Antonello <eantonel@gmail.com>
2017-03-28 15:05:37 +0200

    first tests and escape method integration

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -1,18 +1,75 @@
 <?php
+namespace Tests\AppBundle\Util;
 
-namespace Tests\AppBundle\Controller;
+use AppBundle\Manager\BattleManager;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
+use UserBundle\Entity\User;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class DefaultControllerTest extends WebTestCase
+class BattleManagerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testIndex()
+    public function getMockUser()
     {
-        $client = static::createClient();
+        $mock_user = $this->getMock(User::class, array(
+            'getMonster'
+        ));
 
-        $crawler = $client->request('GET', '/');
+        $mock_user->expects($this->once())
+          ->method('getMonster')
+          ->willReturn([]);
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        return $mock_user;
+    }
+
+    public function getMockToken()
+    {
+        $mock_user = $this->getMockUser();
+
+        $mock_post_auth_guard_token = $this->getMockBuilder(PostAuthenticationGuardToken::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getUser'])
+            ->getMock();
+
+
+        $mock_post_auth_guard_token->expects($this->once())
+          ->method('getUser')
+          ->willReturn($mock_user);
+
+        return $mock_post_auth_guard_token;
+    }
+
+    public function getMockTokenStorageInterface()
+    {
+        $mock_token_storage = $this->getMockBuilder(TokenStorage::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getToken'])
+            ->getMock();
+
+        $mock_token_storage->expects($this->once())
+          ->method('getToken')
+          ->willReturn($this->getMockToken());
+
+        return $mock_token_storage;
+    }
+
+    public function getMockEntityManager()
+    {
+        return $this->getMockBuilder(EntityManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * TEST
+     */
+    public function testSpawnMonster()
+    {
+        $mock_battle_manager = new BattleManager(
+            $this->getMockTokenStorageInterface(),
+            $this->getMockEntityManager()
+        );
+
+        $mock_battle_manager->spawnMonster();
     }
 }
