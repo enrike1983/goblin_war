50b8629d56a0a51350e6bb2f9885d0bb5be6459b
Enrico Antonello <eantonel@gmail.com>
2017-03-30 14:46:35 +0200

    minor fixes

--- src/ApiBundle/Controller/ApiNavigationController.php
+++ src/ApiBundle/Controller/ApiNavigationController.php
@@ -56,22 +56,19 @@ class ApiNavigationController extends FOSRestController
     {
         $battle_manager = $this->container->get('app.battle_manager');
 
-        $fight_info_array = array();
-
         //you are dead. Sorry
         if($battle_manager->youAreDead()) {
             return [
                 'player_status' => BattleManager::PLAYER_IS_DEAD,
-                'status_description' => 'You are dead'
+                'status_description' => 'You are dead',
             ];
         }
 
         //you are fighting. You can't move!
         if($battle_manager->userIsFighting()) {
-            $player_status = BattleManager::BATTLE_IN_FIGHT_STATUS;
-
-            $fight_info_array = [
-                'status_description' => 'A monster appear! You cannot move. Fight (/api/battle/fight) or try to escape (/api/battle/escape)!',
+            return [
+                'player_status' => BattleManager::BATTLE_IN_FIGHT_STATUS,
+                'status_description' => 'A monster appear! You cannot move. Fight or try to escape!',
             ];
         }
 
@@ -82,12 +79,10 @@ class ApiNavigationController extends FOSRestController
         $navigation_manager = $this->container->get('app.navigation_manager');
         $player_manager = $this->container->get('app.player_manager');
 
-        return array_merge(
-            $fight_info_array, [
-                'player_status' => BattleManager::PLAYER_IS_MOVING,
-                'player_profile' => $player_manager->getPlayerProfile(),
-                'navigation' => $navigation_manager->{'go'.ucfirst($direction)}(),
-            ]
-        );
+        return [
+            'player_status' => BattleManager::PLAYER_IS_MOVING,
+            'player_profile' => $player_manager->getPlayerProfile(),
+            'navigation' => $navigation_manager->{'go'.ucfirst($direction)}(),
+        ];
     }
 }
